b8180fd20a6ae27df63484cce9a4720997e579ac
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-12-04 13:08:58 -0800

    Add files via upload

---
+++ Receipts.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MAK
+ */
+public void actionPerformed(AcetionEvent ae) {
+    if (ae.getActionCommand() == /*Whatever button*/.getActionCommand()) {
+try {
+fileWriter = new FileWriter("Receipt.txt");
+fileWriter.write(/*Whatever label name*/.getText() + " - " + /*whatever text field*/.getText);
+fileWriter
+}
+}
+    
+}
