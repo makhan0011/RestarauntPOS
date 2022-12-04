335a04e408257ec706ca55d2cae2b7787552aa33
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-12-04 12:56:45 -0800

    Add files via upload

---
+++ MenuCostsServer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MAK
+ */
+public class MenuCosts extends /*....*/ {
+    double costHashBrowns = 1.00;
+    double costPancake = 1.00;
+    double costHamburger = 1.00;
+    double costHotDog = 1.00;
+    double costIceCream = 1.00;
+    double costWater = 1.00;
+    double costSoda = 1.00;
+    double costCoffee = 1.00;
+    double costOrangeJuice = 1.00;
+    double costVodka = 1.00;
+
+    double[] itemCost = new double [10];
+    double totalCost, subTotal, change;
+    final double taxes = 0.090;
+    subTotal = itemCost[0] + itemCost[1] + itemCost[2] + itemCost[3] + itemCost[4] + itemCost[5] + itemCost[6] + itemCost[7] + itemCost[8] + itemCost[9];
+    totalCost = subTotal * taxes;
+
+    
+    
+}
